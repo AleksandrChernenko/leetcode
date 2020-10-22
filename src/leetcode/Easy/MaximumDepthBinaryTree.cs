8fb44b0e8ec2c6df969a81c3b65cb4adbc8c3934
Aleksandr Chernenko <aleksandr.chernenko@softwarium.net>
2020-10-22 20:02:00 +0300

    104. Maximum Depth of Binary Tree

---
+++ src/leetcode/Easy/MaximumDepthBinaryTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace leetcode.Easy
+{
+    /// <summary>
+    /// 104. Maximum Depth of Binary Tree
+    /// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+    /// </summary>
+    class MaximumDepthBinaryTree
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            var depth = 1;
+            var left = 0;
+            var right = 0;
+            if (root.left != null)
+                left += MaxDepth(root.left);
+            if (root.right != null)
+                right += MaxDepth(root.right);
+
+            return left > right ? depth + left : depth + right;
+        }
+    }
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
